00b7fde75e74944bb328c06779d8d5b744a6e86b
Stanislas Ormières <stan@stormier.ninja>
2020-03-31 20:49:09 +0200

    :heavy_plus_sign: Add Fontawesome

--- client/main.js
+++ client/main.js
@@ -1,1 +1,7 @@
 import './main.css'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faArchway, faAd, faBrain } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArchway, faAd, faBrain) 
+
+dom.i2svg() 
